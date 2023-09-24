186c6ebc7d52bf7b7ef543b4320d0b50f627cffd
Alexpron2004 <113039285+Alexpron2004@users.noreply.github.com>
2023-09-24 09:40:48 +0300

    main.go

---
+++ list/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	list := &List{}
+	list.Add(5)
+	list.Add(2)
+	list.Add(3)
+	list.Add(5)
+	list.Add(4)
+	list.Add(5)
+	fmt.Println("Initial List: ")
+	listPrint(list)
+	fmt.Println("Len List: ")
+	listLen(list)
+	fmt.Println("Get all: ")
+	list.GetAll()
+	fmt.Println("Get by index: ")
+	list.GetByIndex(500)
+	fmt.Println("Get by value: ")
+	list.GetByValue(5)
+	//fmt.Println("Get All by value: ")
+	//list.GetAllByValue(5)
+	//fmt.Println("RemoveByValue: ") Почему-то не работает
+	//list.RemoveByValue(3)
+	//listPrint(list)
+	//fmt.Println("RemoveByIndex: ")
+	//list.RemoveByIndex(500000) Почему-то зависает
+	//listPrint(list)
+	//fmt.Println("RemoveAllByValue: ")
+	//list.RemoveAllByValue(3)
+	//listPrint(list)
+	fmt.Println("Clear List: ")
+	Clear(list)
+	listPrint(list)
+	fmt.Println("Clear List len: ")
+	listLen(list)
+
+}
