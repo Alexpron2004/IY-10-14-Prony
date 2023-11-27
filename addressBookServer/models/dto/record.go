1e2993c5a91f2379ae36069838a5387c2a3de61a
Alexpron2004 <113039285+Alexpron2004@users.noreply.github.com>
2023-11-27 23:40:39 +0300

    Add files via upload

---
+++ addressBookServer/models/dto/record.go
@@ -0,0 +1,10 @@
+package main
+
+type Record struct {
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	LastName   string `json:"last_name"`
+	MiddleName string `json:"middle_name,omitempty"`
+	Phone      string `json:"phone"`
+	Address    string `json:"address"`
+}
