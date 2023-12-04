1229bed5e265a12e909c453b42f48e9beeea47f6
Alexpron2004 <113039285+Alexpron2004@users.noreply.github.com>
2023-12-04 16:30:15 +0300

    Update phone.go

--- addressBookServer/pkg/phone.go
+++ addressBookServer/pkg/phone.go
@@ -1,41 +1,22 @@
 package pkg
 
-func PhoneNormalize(phone string) (normalizedPhone string, err error) {
-	return
-}
-
-package main
-
 import (
- "fmt"
- "regexp"
- "strings"
+	"regexp"
+	"strings"
 )
 
-func formatPhoneNumber(phoneNumber string) string {
- // Удаление всех символов, кроме цифр
- re := regexp.MustCompile("[^0-9]+")
- digitsOnly := re.ReplaceAllString(phoneNumber, "")
+func PhoneNormalize(phone string) (normalizedPhone string, err error) {
+	// Удаление всех символов, кроме цифр
+	re := regexp.MustCompile("[^0-9]+")
+	normalized := re.ReplaceAllString(phone, "")
 
- // Приведение номера к единому формату
- if strings.HasPrefix(digitsOnly, "8") {
-  digitsOnly = "7" + digitsOnly[1:]
- } else if strings.HasPrefix(digitsOnly, "7") {
-  digitsOnly = "7" + digitsOnly[1:]
- } else if strings.HasPrefix(digitsOnly, "+7") {
-  digitsOnly = "7" + digitsOnly[2:]
- }
+	// Если номер телефона начинается с "8", заменяем на "7" (для России)
+	if strings.HasPrefix(normalized, "8") {
+		normalized = "7" + normalized[1:]
+	} else if strings.HasPrefix(normalized, "+7") {
+		// Если номер начинается с "+7", удаляем "+"
+		normalized = normalized[1:]
+	}
 
- return digitsOnly
-}
-
-func main() {
- phoneNumber1 := "+7 (999) 123-45-67"
- phoneNumber2 := "8-999-123-45-67"
-
- formattedPhoneNumber1 := formatPhoneNumber(phoneNumber1)
- formattedPhoneNumber2 := formatPhoneNumber(phoneNumber2)
-
- fmt.Println(formattedPhoneNumber1)
- fmt.Println(formattedPhoneNumber2)
+	return normalized, nil
 }
